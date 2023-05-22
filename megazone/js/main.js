dedbbd1f5833ae3b738232fb545003481493e375
jjang051globalit <zerox2519@gmail.com>
2023-05-22 18:17:24 +0900

    0522

---
+++ megazone/js/main.js
@@ -0,0 +1,7 @@
+new Swiper(".tech .mask", {
+  slidesPerView: "2",
+  //한번에 볼 수 있는 갯수 4개에 맞춰 li의 크기를 줄인다.
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+});
--- sleepeye/js/main.js
+++ sleepeye/js/main.js
@@ -8,3 +8,11 @@ gnbList.forEach((item, idx) => {
 header.addEventListener("mouseleave", () => {
   header.classList.remove("on");
 });
+
+new Swiper(".media .mask", {
+  slidesPerView: "auto",
+  //한번에 볼 수 있는 갯수 4개에 맞춰 li의 크기를 줄인다.
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+});
